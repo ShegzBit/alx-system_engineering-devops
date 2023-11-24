e0d3f652c8ef41b936cf2dd49e48deb5b65caa0e
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-24 16:50:01 +0100

    tried something else for task 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
 # create a file in puppet
-file { '/tmp/school':
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+
+file { 'title':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet'
 }
