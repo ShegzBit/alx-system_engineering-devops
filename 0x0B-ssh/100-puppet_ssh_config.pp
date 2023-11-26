9846272bebe7385c1826bb3c58a117d6fa923121
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-26 11:21:59 +0100

    solved task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# a puppet script to configure our ssh_config file
+
+file { '/root/.ssh/config':
+  ensure  => 'file',
+  content => '# A puppeteer for my configuration file
+Host *
+    IdentityFile ~/.ssh/school
+    PassWordAuthentication no
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes',
+}
