f961fef6bc0032b81fd2da822ffdba157602fef3
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-26 11:32:14 +0100

    solved task 100<7>

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file { '/root/.ssh/config':
   ensure  => 'present',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0600',
   content => '# A puppeteer for my configuration file
 Host *
     IdentityFile ~/.ssh/school
     PassWordAuthentication no
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
 '
 }
