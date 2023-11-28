61cd7f3f09b574b78767603ef63fbfba032dc558
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-29 00:54:35 +0100

    corrected task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # installing a package using puppet with pip3 provider
 
+package { 'Werkzeug':
+  ensure   => 2.1.0,
+  name     => 'Werkzeug',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
