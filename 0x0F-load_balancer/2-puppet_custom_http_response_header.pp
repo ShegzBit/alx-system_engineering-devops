6cc06b48fa936fbabd09b090ae0a84d19099fb11
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-06 03:39:47 +0100

    testing only header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,52 +12,7 @@ package {'nginx':
   provider => 'apt',
 }
 
-file {'/var/www/html/index.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => "Hello World!\n",
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
-
 $config = '/etc/nginx/sites-available/default'
-$redirect='\
-        location /redirect {\
-                return 301 https://www.google.com/;\
-        }\n'
-
-exec {'redirect_me':
-  command => "sed -i '/server_name _;/a\\ ${redirect}' ${config}",
-  path    => ['/usr/bin', '/bin'],
-}
-
-file {'/var/www/html/404.html':
-  ensure  => 'present',
-  path    => '/var/www/html/404.html',
-  content => "Ceci n'est pas une page\n",
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
-
-$f04 = "\terror_page 404 /404.html;"
-$r2404 = '\
-	location /404.html {\
-		root /var/www/html;\
-		internal;\
-	}\n'
-
-exec {'custom_error':
-  command => "sed -i '/server_name _;/a\\ ${r2404}' ${config}",
-  path    => ['/usr/bin/', '/bin'],
-}
-
-exec {'custom_error_contd':
-  command => "sed -i '/server_name _;/a\\ ${f04}' ${config}",
-  path    => ['/usr/bin', '/bin'],
-}
-
 $header = '\n\tadd_header X-Served-By \$hostname;'
 
 exec {'configure_header':
