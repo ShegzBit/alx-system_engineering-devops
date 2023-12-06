b840ccf96bd75e45c15b39e8d33ab813c62ff4a7
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-06 02:35:52 +0100

    task 7 puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# puppet to install nginx and configure
+
+exec {'update':
+  command => 'apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+package {'nginx installation':
+  ensure   => installed,
+  name     => 'nginx',
+  provider => 'apt',
+}
+
+file {'/var/www/html/index.html':
+  ensure  => 'present',
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+
+$redirect = '\
+	location /redirect_me {\
+		return 301 https://www.google.com;\
+	}'
+
+$config = '/etc/nginx/sites-available/default'
+
+exec {'append':
+  command => "sed -i '/server_name _;/a\\ ${redirect}' ${config}",
+  path    => ['/usr/bin/', '/bin'],
+}
+
+service {'restart':
+  ensure     => running,
+  name       => 'nginx',
+  enable     => 'true',
+}
