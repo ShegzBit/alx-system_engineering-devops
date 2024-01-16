ffeaad054c26819ae3010bdbc605b7ceb8466054
ShegzBit <feranmiawotubo16@gmail.com>
2024-01-16 19:49:29 +0100

    solve task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+A python program that scrapes reddit for the number of subscriber
+of a subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscriber a subreddit has
+
+    subreddit: subreddit to scrape for
+    """
+    try:
+        body = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json')
+        return body.json()['data']['subscribers']
+    except KeyError:
+        return 0
