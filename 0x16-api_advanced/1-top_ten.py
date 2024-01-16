6af3b84d2325e2b7f1aa20437f9ae90ee44f25b8
ShegzBit <feranmiawotubo16@gmail.com>
2024-01-17 00:10:35 +0100

    solved task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+A module that prints the title of the top ten most popular posts from
+a subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the top ten post of a subreddit
+
+    subreddit: subreddit to scrape
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/top/.json?limit=10'
+    response = requests.get(url, headers={'User-Agent': 'My-Agent'},
+                            allow_redirects=False)
+    post_list = response.json().get('data').get('children')
+
+    for post in post_list:
+        print(post.get('data').get('title'))
