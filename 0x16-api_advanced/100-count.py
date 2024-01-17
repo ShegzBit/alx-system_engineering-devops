96d23737aa8c82a43bdb378e0b0bfc0fad64c4b9
ShegzBit <feranmiawotubo16@gmail.com>
2024-01-17 01:20:13 +0100

    prepared all templates

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+A module doc template
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    A function doc template
+    """
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+A module doc template
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    A function doc template
+    """
+    return None
